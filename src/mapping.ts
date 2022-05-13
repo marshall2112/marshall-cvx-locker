0f5a7b4a2cbec7e7a3ca2b36ad10ddd31fca5157
marshall <cameronmarshall2112@gmail.com>
2022-05-13 16:01:30 -0400

    Fixes

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,7 +5,8 @@ export function handleStaked(event: Staked): void {
   //    event Staked(address indexed _user, uint256 indexed _epoch, uint256 _paidAmount, uint256 _lockedAmount, uint256 _boostedAmount);
   let cvxLock = CvxLock.load(event.transaction.hash.toHex());
 
-  if (cvxLock !== null) {
+  if (cvxLock === null) {
+    cvxLock = new CvxLock(event.transaction.hash.toHex());
     cvxLock.user = event.params._user;
     cvxLock.lockAmount = event.params._lockedAmount;
     cvxLock.epoch = event.params._epoch;
